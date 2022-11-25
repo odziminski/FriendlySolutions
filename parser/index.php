e8275a3261a98a1c06dd46eba81aeb404aa78f5c
odziminski <o.dziminski@gmail.com>
2022-11-25 12:42:47 +0100

    add regular expression method to retrieve fields from address string

--- parser/index.php
+++ parser/index.php
@@ -27,12 +27,17 @@ class Parser
         } else return false;
     }
 
-    public function main()
+    public function main(): void
     {
         $woNumber = $this->getData('wo_number');
         $poNumber = $this->getData('po_number');
         $scheduledDate = $this->prepareDatetime($this->getData('scheduled_date'));
-        var_dump($scheduledDate);
+        $customer = $this->getData('customer');
+        $trade = $this->getData('trade');
+        $nte = preg_replace("/[^\d,.]/", "", $this->getData('nte'));
+        $storeID = $this->getData('location_name');
+        $address = $this->getAddressFields($this->getData('location_address'));
+        $locationPhone = floatval(preg_replace('/\D+/', '', $this->getData('location_phone')));
     }
 
     public function prepareDatetime($scheduledDate): string
@@ -40,6 +45,21 @@ class Parser
         $scheduledDate = preg_replace('#\s+#', ' ', $scheduledDate);
         $carbon = new Carbon($scheduledDate, 'America/New_York');
         return $carbon->format('Y-m-d H:i');
+    }
+
+    public function getAddressFields($address): bool|array
+    {
+        if (isset($address) && !empty($address)) {
+            $address = preg_replace('#[\s]+#', ' ', trim($address)); //strip more than 1 whitespace
+            $numbers = preg_split('/\D/', $address, -1, PREG_SPLIT_NO_EMPTY);
+            $letters = preg_split('/[^a-zA-Z]/', $address, -1, PREG_SPLIT_NO_EMPTY);
+
+            $address = $letters[0] . " " . $letters[1] . " " . $numbers[0];
+            $city = $letters[2];
+            $state = $letters[3];
+            $zip = $numbers[1];
+            return ['address' => $address, 'city' => $city, 'state' => $state, 'zip' => $zip];
+        } else return false;
     }
 }
 
