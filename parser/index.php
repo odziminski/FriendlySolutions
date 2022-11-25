23735b903a3452cd01055fdd5bd99fa2f6b55cd8
odziminski <o.dziminski@gmail.com>
2022-11-25 11:20:30 +0100

    add Parser class, add HTML DOM loading function

--- parser/index.php
+++ parser/index.php
@@ -1,1 +1,16 @@
 <?php
+
+class Parser
+{
+
+    public function loadHtmlDom($path)
+    {
+        $dom = new DOMDocument();
+        $html = file_get_contents($path);
+        $dom->validateOnParse = true;
+        $dom->loadHTML($html, LIBXML_NOERROR);
+
+        $dom->preserveWhiteSpace = false;
+        return $dom;
+    }
+}
