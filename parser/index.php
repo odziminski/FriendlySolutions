ff676d56191ec71cccc11d309547b8a93bb29469
odziminski <o.dziminski@gmail.com>
2022-11-25 11:57:01 +0100

    add Carbon package and format the retrieved date

--- parser/index.php
+++ parser/index.php
@@ -1,9 +1,13 @@
 <?php
 
+use Carbon\Carbon;
+
+require 'vendor/autoload.php';
+
 class Parser
 {
 
-    public function loadHtmlDom($path)
+    public function loadHtmlDom($path): DOMDocument
     {
         $dom = new DOMDocument();
         $html = file_get_contents($path);
@@ -13,4 +17,31 @@ class Parser
         $dom->preserveWhiteSpace = false;
         return $dom;
     }
+
+    public function getData($id): ?string
+    {
+        $dom = $this->loadHtmlDom('wo_for_parse.html');
+        $element = $dom->getElementById($id);
+        if ($element) {
+            return $element?->nodeValue;
+        } else return false;
+    }
+
+    public function main()
+    {
+        $woNumber = $this->getData('wo_number');
+        $poNumber = $this->getData('po_number');
+        $scheduledDate = $this->prepareDatetime($this->getData('scheduled_date'));
+        var_dump($scheduledDate);
+    }
+
+    public function prepareDatetime($scheduledDate): string
+    {
+        $scheduledDate = preg_replace('#\s+#', ' ', $scheduledDate);
+        $carbon = new Carbon($scheduledDate, 'America/New_York');
+        return $carbon->format('Y-m-d H:i');
+    }
 }
+
+$parser = new Parser();
+$parser -> main();
